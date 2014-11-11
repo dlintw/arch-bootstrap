fbe0188d9ef6721578ec491f5e9cfa4ee916ae5a
Daniel YC Lin <dlin.tw@gmail>
2014-11-11 10:04:29 +0800

    read /var/abs/local/pacman/pacman.conf if /var/abs/core not found

--- get-pacman-dependencies.sh
+++ get-pacman-dependencies.sh
@@ -3,7 +3,7 @@ set -e -u -o pipefail
 
 shared_dependencies() {
   local EXECUTABLE=$1
-  for PACKAGE in $(ldd "$EXECUTABLE" | grep "=> /" | awk '{print $3}'); do 
+  for PACKAGE in $(ldd "$EXECUTABLE" | grep "=> /" | awk '{print $3}'); do
     LC_ALL=c pacman -Qo $PACKAGE
   done | awk '{print $5}'
 }
@@ -16,9 +16,16 @@ pkgbuild_dependencies() {
     echo "$DEPEND" | sed "s/[>=<].*$//"
   done | grep -v "$EXCLUDE"
 }
-
+f=/var/abs/core/pacman/PKGBUILD
+if [ ! -r $f ] ; then
+  f=/var/abs/local/pacman/PKGBUILD
+fi
+if [ ! -r $f ] ; then
+  echo "Err: missing pacman/PKGBUILD in /var/abs/core or /var/abs/local"
+  exit 1
+fi
 # Main
-{ 
+{
   shared_dependencies "/usr/bin/pacman"
-  pkgbuild_dependencies "/var/abs/core/pacman/PKGBUILD" "bash"
+  pkgbuild_dependencies $f "bash"
 } | sort -u | xargs
